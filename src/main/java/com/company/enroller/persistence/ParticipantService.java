7c1f0d6f70ceee9a2e95067b7896e1dc70990e99
Anna Gorzanowska <anna.gorzanowska@gmail.com>
2021-04-24 11:49:49 +0200

    database management"

--- src/main/java/com/company/enroller/persistence/ParticipantService.java
+++ src/main/java/com/company/enroller/persistence/ParticipantService.java
@@ -2,6 +2,7 @@ package com.company.enroller.persistence;
 
 import java.util.Collection;
 
+import org.hibernate.Transaction;
 import org.springframework.stereotype.Component;
 
 import com.company.enroller.model.Participant;
@@ -9,14 +10,32 @@ import com.company.enroller.model.Participant;
 @Component("participantService")
 public class ParticipantService {
 
-	DatabaseConnector connector;
+    DatabaseConnector connector;
 
-	public ParticipantService() {
-		connector = DatabaseConnector.getInstance();
-	}
+    public ParticipantService() {
+        connector = DatabaseConnector.getInstance();
+    }
 
-	public Collection<Participant> getAll() {
-		return connector.getSession().createCriteria(Participant.class).list();
-	}
+    public Collection<Participant> getAll() {
+        return connector.getSession().createCriteria(Participant.class).list();
+    }
+
+    public Participant findByLogin(String login) {
+        Participant participant = (Participant) connector.getSession().get(Participant.class, login);
+
+        return participant;
+    }
+
+    public void add(Participant participant) {
+        Transaction transaction = this.connector.getSession().beginTransaction();
+        connector.getSession().save(participant);
+        transaction.commit();
+    }
+
+    public void remove(Participant participant) {
+        Transaction transaction = this.connector.getSession().beginTransaction();
+        connector.getSession().delete(participant);
+        transaction.commit();
+    }
 
 }
